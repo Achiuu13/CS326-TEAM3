952cdf9b88736bec5c18e5757303b6b61649c505
Abhisheha Muthuswamy <abhishehamut@umass.edu>
2026-07-19 21:18:03 -0400

    added two domain-specific routes

--- server.js
+++ server.js
@@ -7,6 +7,16 @@ app.get('/', (req, res) => {
   res.send('<h1>Welcome to StudyHub</h1>');
 });
 
+//list of study groups
+app.get('/groups', (req, res) => {
+  res.send('<h1>Available Study Groups</h1>');
+});
+
+//page to create a study group
+app.get('/create-group', (req, res) => {
+  res.send('<h1>Create a Study Group</h1>');
+});
+
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });

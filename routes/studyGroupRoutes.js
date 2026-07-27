67aac94dadb71a2a1310b51bcadc56e395a44d42
Achiuu13 <achiu@umass.edu>
2026-07-27 02:58:16 +0000

    complete routes and views for study group creation

--- routes/studyGroupRoutes.js
+++ routes/studyGroupRoutes.js
@@ -1,4 +1,10 @@
 import { Router } from "express";
 import * as studyGroupController from "../controllers/studyGroupController.js";
 
+const router = Router();
+
+router.get('/', studyGroupController.index);
+router.get('/new', studyGroupController.showCreateForm);
+router.post('/', studyGroupController.create);
+
 export default router;
